950d0dd5ae4a5b37c450b7803c9097abd9ee4170
behnamsaba <behnams71@gmail.com>
2023-03-06 15:01:16 -0800

    class to className fixed

--- part-3/Person.js
+++ part-3/Person.js
@@ -1,18 +1,18 @@
 const Person = ({ name , age , ... props }) => {
     let legalAge = age > 18 ? 'please go vote' : "you must be 18";
-    let hobbies = props.hobbies.map(hobby => <li class="list-group-item">{hobby}</li>);
+    let hobbies = props.hobbies.map(hobby => <li className="list-group-item">{hobby}</li>);
 
     return(
-    <div class="card">
-        <div class="card-body">
-        <p class="text-primary">Learn some information about this person</p>
-        <ul class="list-group">
-            <li class="list-group-item">name: {name.slice(0,6)}</li>
-            <li class="list-group-item">age : {age}</li>
+    <div className="card">
+        <div className="card-body">
+        <p className="text-primary">Learn some information about this person</p>
+        <ul className="list-group">
+            <li className="list-group-item">name: {name.slice(0,6)}</li>
+            <li className="list-group-item">age : {age}</li>
         </ul>
         <h3>{legalAge}</h3>
 
-        <ul class="list-group">
+        <ul className="list-group">
             {hobbies}     
         </ul>
         </div>
